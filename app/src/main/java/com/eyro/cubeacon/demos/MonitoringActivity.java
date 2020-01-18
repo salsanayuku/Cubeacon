c06996b8937e9708b0d3f832f0b76a3022976cdd
Xalam <ganufvyandana@gmail.com>
2020-01-18 14:04:54 +0700

    vibrator & tts

--- app/src/main/java/com/eyro/cubeacon/demos/MonitoringActivity.java
+++ app/src/main/java/com/eyro/cubeacon/demos/MonitoringActivity.java
@@ -1,9 +1,17 @@
 package com.eyro.cubeacon.demos;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.RemoteException;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.eyro.cubeacon.CBBeacon;
@@ -13,8 +21,11 @@ import com.eyro.cubeacon.CBServiceListener;
 import com.eyro.cubeacon.Cubeacon;
 import com.eyro.cubeacon.MonitoringState;
 import com.eyro.cubeacon.SystemRequirementManager;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Locale;
 
@@ -31,10 +42,99 @@ public class MonitoringActivity extends AppCompatActivity implements CBServiceLi
     FirebaseDatabase database;
     DatabaseReference myRef;
 
+    TextToSpeech textVoice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monitoring);
+
+        //Set Vibrator And Speech
+        final Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        textVoice = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int ttsLang = textVoice.setLanguage(new Locale("id", "ID"));
+
+                    if (ttsLang == TextToSpeech.LANG_MISSING_DATA
+                            || ttsLang == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "The Language is not supported!");
+                    } else {
+                        Log.i("TTS", "Language Supported.");
+                    }
+                    Log.i("TTS", "Initialization success.");
+                } else {
+                    Toast.makeText(getApplicationContext(), "TTS Initialization failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String sensorL = dataSnapshot.child("sensor_left").getValue(String.class);
+                String sensorR = dataSnapshot.child("sensor_right").getValue(String.class);
+
+                Integer sensor_Lint = Integer.parseInt(sensorL);
+                Integer sensor_Rint = Integer.parseInt(sensorR);
+
+                if (sensor_Lint <= 50){
+                    String textLeft = "Ada Benda, Geser Ke Kanan Sedikit";
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        textVoice.speak(textLeft, TextToSpeech.QUEUE_FLUSH, null, null);
+                    }else{
+                        textVoice.speak(textLeft, TextToSpeech.QUEUE_FLUSH, null);
+                    }
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        vibrator.vibrate(VibrationEffect.createOneShot(4000, VibrationEffect.DEFAULT_AMPLITUDE));
+                    } else {
+                        //deprecated in API 26
+                        vibrator.vibrate(4000);
+                    }
+
+                }
+                if(sensor_Rint <= 50){
+                    String textRight = "Ada Benda, Geser Ke Kiri Sedikit";
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        textVoice.speak(textRight, TextToSpeech.QUEUE_FLUSH, null, null);
+                    }else{
+                        textVoice.speak(textRight, TextToSpeech.QUEUE_FLUSH, null);
+                    }
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        vibrator.vibrate(VibrationEffect.createOneShot(4000, VibrationEffect.DEFAULT_AMPLITUDE));
+                    } else {
+                        //deprecated in API 26
+                        vibrator.vibrate(4000);
+                    }
+                }
+                if(sensor_Lint <= 50 && sensor_Rint < 50) {
+                    String textAll = "Ada Benda Di Depan Anda";
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        textVoice.speak(textAll, TextToSpeech.QUEUE_FLUSH, null, null);
+                    }else{
+                        textVoice.speak(textAll, TextToSpeech.QUEUE_FLUSH, null);
+                    }
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        vibrator.vibrate(VibrationEffect.createOneShot(4000, VibrationEffect.DEFAULT_AMPLITUDE));
+                    } else {
+                        //deprecated in API 26
+                        vibrator.vibrate(4000);
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
         // get beacon parcelable from intent activity
         beacon = getIntent().getParcelableExtra(INTENT_BEACON);
@@ -88,7 +188,10 @@ public class MonitoringActivity extends AppCompatActivity implements CBServiceLi
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
+        if (textVoice != null) {
+            textVoice.stop();
+            textVoice.shutdown();
+        }
         // disconnect from Cubeacon service when this activity destroyed
         cubeacon.disconnect(this);
     }
